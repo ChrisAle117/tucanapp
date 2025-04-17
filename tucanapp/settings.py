c9a2278d3d80295038be034aea2693dfe453c385
BojitaNoir <20213tn094@utez.edu.mx>
2025-04-17 01:34:35 -0600

    [models de equipo actualizado yiyi]
    Models de equipo, settings y urls de tucanapp corregidos o bueno agregando lo de las imagenes xd

--- tucanapp/settings.py
+++ tucanapp/settings.py
@@ -16,7 +16,7 @@ pymysql.install_as_MySQLdb()
 # from logging.handlers import RotatingFileHandler
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -189,3 +189,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- tucanapp/urls.py
+++ tucanapp/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from app.views import *
 from users.views import * 
+from django.conf import settings
+from django.conf.urls.static import static  # <- esta línea importa el helper
 
 urlpatterns = [
     path('usuarios/', include('users.urls')),
@@ -9,7 +11,12 @@ urlpatterns = [
     path('deportes/', include('deporte.urls')),
     path('config_deporte/', include('configuracion_deporte.urls')),
     path('posicion/', include('posicion.urls')),
-    path('' , index, name='index'),
-    path('login/' , login_view, name='login'),
-    path('logout/' , logout_view, name='logout'),
+    path('eventos/', include('evento.urls')),
+    path('', index, name='index'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
 ]
+
+# Esto sirve las imágenes en desarrollo
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
