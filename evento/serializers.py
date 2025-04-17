2a99a1cf8c54c1adbff969dc0b92c4a47bea7d4f
Aserejex22 <132949414+Aserejex22@users.noreply.github.com>
2025-04-17 01:28:33 -0600

    Cambios y validaciones de eventos
    Hola x2

--- evento/serializers.py
+++ evento/serializers.py
@@ -4,4 +4,11 @@ from rest_framework import serializers
 class EventoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Evento
-        fields = '__all__'
+        fields = ['id', 'nombre', 'fecha', 'equipo1', 'equipo2', 'puntos_equipo1', 'puntos_equipo2', 'resultado']
+
+    def validate(self, data):
+        if data['equipo1'].deporte != data['equipo2'].deporte:
+            raise serializers.ValidationError("Los equipos deben ser del mismo deporte")
+        if data['equipo1'] == data['equipo2']:
+            raise serializers.ValidationError("Un equipo no puede jugar contra sí mismo")
+        return data
